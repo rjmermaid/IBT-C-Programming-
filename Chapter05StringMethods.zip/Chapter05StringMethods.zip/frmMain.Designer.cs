653a330215e24c3a7020b540142f11322d6fb435
rjmermaid <bashenovjr@gmail.com>
2021-04-14 20:41:33 +0600

    Created Chapter05StringMethods program

---
+++ Chapter05StringMethods.zip/Chapter05StringMethods.zip/frmMain.Designer.cs
@@ -0,0 +1,414 @@
+﻿
+namespace Chapter05StringMethods.zip
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtToUpper = new System.Windows.Forms.TextBox();
+            this.txtToLower = new System.Windows.Forms.TextBox();
+            this.txtSearchChar = new System.Windows.Forms.TextBox();
+            this.txtLastChar = new System.Windows.Forms.TextBox();
+            this.txtStartIndex = new System.Windows.Forms.TextBox();
+            this.txtRemove = new System.Windows.Forms.TextBox();
+            this.txtReplaceChars = new System.Windows.Forms.TextBox();
+            this.txtReplaceResult = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtSearchIndex = new System.Windows.Forms.TextBox();
+            this.txtLastIndexOf = new System.Windows.Forms.TextBox();
+            this.txtEndIndex = new System.Windows.Forms.TextBox();
+            this.txtSubstringResult = new System.Windows.Forms.TextBox();
+            this.txtRemoveResult = new System.Windows.Forms.TextBox();
+            this.txtReplaceWith = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "String to test:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "String.Length:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "String.ToUpper():";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "String.ToLower():";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Search for character:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Search for last character:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 196);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(139, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Extract substring from index:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(45, 235);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(106, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "txtInput.Remove() = :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(121, 269);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(30, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Find:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(41, 301);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(106, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "txtInput.Replace() = :";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(190, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(495, 20);
+            this.txtInput.TabIndex = 10;
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(190, 40);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 11;
+            // 
+            // txtToUpper
+            // 
+            this.txtToUpper.Location = new System.Drawing.Point(190, 70);
+            this.txtToUpper.Name = "txtToUpper";
+            this.txtToUpper.Size = new System.Drawing.Size(495, 20);
+            this.txtToUpper.TabIndex = 12;
+            // 
+            // txtToLower
+            // 
+            this.txtToLower.Location = new System.Drawing.Point(190, 105);
+            this.txtToLower.Name = "txtToLower";
+            this.txtToLower.Size = new System.Drawing.Size(495, 20);
+            this.txtToLower.TabIndex = 13;
+            // 
+            // txtSearchChar
+            // 
+            this.txtSearchChar.Location = new System.Drawing.Point(190, 135);
+            this.txtSearchChar.Name = "txtSearchChar";
+            this.txtSearchChar.Size = new System.Drawing.Size(48, 20);
+            this.txtSearchChar.TabIndex = 14;
+            // 
+            // txtLastChar
+            // 
+            this.txtLastChar.Location = new System.Drawing.Point(190, 163);
+            this.txtLastChar.Name = "txtLastChar";
+            this.txtLastChar.Size = new System.Drawing.Size(48, 20);
+            this.txtLastChar.TabIndex = 15;
+            // 
+            // txtStartIndex
+            // 
+            this.txtStartIndex.Location = new System.Drawing.Point(190, 195);
+            this.txtStartIndex.Name = "txtStartIndex";
+            this.txtStartIndex.Size = new System.Drawing.Size(48, 20);
+            this.txtStartIndex.TabIndex = 16;
+            // 
+            // txtRemove
+            // 
+            this.txtRemove.Location = new System.Drawing.Point(190, 232);
+            this.txtRemove.Name = "txtRemove";
+            this.txtRemove.Size = new System.Drawing.Size(100, 20);
+            this.txtRemove.TabIndex = 17;
+            // 
+            // txtReplaceChars
+            // 
+            this.txtReplaceChars.Location = new System.Drawing.Point(190, 266);
+            this.txtReplaceChars.Name = "txtReplaceChars";
+            this.txtReplaceChars.Size = new System.Drawing.Size(100, 20);
+            this.txtReplaceChars.TabIndex = 18;
+            // 
+            // txtReplaceResult
+            // 
+            this.txtReplaceResult.Location = new System.Drawing.Point(190, 298);
+            this.txtReplaceResult.Name = "txtReplaceResult";
+            this.txtReplaceResult.Size = new System.Drawing.Size(495, 20);
+            this.txtReplaceResult.TabIndex = 19;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(370, 134);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(140, 13);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "txtInput.Text.IndexOf(\" \", ) =";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(370, 170);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(154, 13);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "txtInput.Text.LastIndexOf(\" \") =";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(255, 198);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(119, 13);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "for this many characters";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(449, 199);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(137, 13);
+            this.label14.TabIndex = 23;
+            this.label14.Text = "txrInput.Text.Substring( , ) =";
+            // 
+            // txtSearchIndex
+            // 
+            this.txtSearchIndex.Location = new System.Drawing.Point(540, 131);
+            this.txtSearchIndex.Name = "txtSearchIndex";
+            this.txtSearchIndex.Size = new System.Drawing.Size(76, 20);
+            this.txtSearchIndex.TabIndex = 24;
+            // 
+            // txtLastIndexOf
+            // 
+            this.txtLastIndexOf.Location = new System.Drawing.Point(540, 167);
+            this.txtLastIndexOf.Name = "txtLastIndexOf";
+            this.txtLastIndexOf.Size = new System.Drawing.Size(76, 20);
+            this.txtLastIndexOf.TabIndex = 25;
+            // 
+            // txtEndIndex
+            // 
+            this.txtEndIndex.Location = new System.Drawing.Point(390, 195);
+            this.txtEndIndex.Name = "txtEndIndex";
+            this.txtEndIndex.Size = new System.Drawing.Size(40, 20);
+            this.txtEndIndex.TabIndex = 26;
+            // 
+            // txtSubstringResult
+            // 
+            this.txtSubstringResult.Location = new System.Drawing.Point(585, 196);
+            this.txtSubstringResult.Name = "txtSubstringResult";
+            this.txtSubstringResult.Size = new System.Drawing.Size(100, 20);
+            this.txtSubstringResult.TabIndex = 27;
+            // 
+            // txtRemoveResult
+            // 
+            this.txtRemoveResult.Location = new System.Drawing.Point(330, 232);
+            this.txtRemoveResult.Name = "txtRemoveResult";
+            this.txtRemoveResult.Size = new System.Drawing.Size(355, 20);
+            this.txtRemoveResult.TabIndex = 28;
+            // 
+            // txtReplaceWith
+            // 
+            this.txtReplaceWith.Location = new System.Drawing.Point(410, 266);
+            this.txtReplaceWith.Name = "txtReplaceWith";
+            this.txtReplaceWith.Size = new System.Drawing.Size(100, 20);
+            this.txtReplaceWith.TabIndex = 29;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(318, 269);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(72, 13);
+            this.label15.TabIndex = 30;
+            this.label15.Text = "Replace with:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "&Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(610, 345);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 32;
+            this.button2.Text = "&Close";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 375);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.txtReplaceWith);
+            this.Controls.Add(this.txtRemoveResult);
+            this.Controls.Add(this.txtSubstringResult);
+            this.Controls.Add(this.txtEndIndex);
+            this.Controls.Add(this.txtLastIndexOf);
+            this.Controls.Add(this.txtSearchIndex);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtReplaceResult);
+            this.Controls.Add(this.txtReplaceChars);
+            this.Controls.Add(this.txtRemove);
+            this.Controls.Add(this.txtStartIndex);
+            this.Controls.Add(this.txtLastChar);
+            this.Controls.Add(this.txtSearchChar);
+            this.Controls.Add(this.txtToLower);
+            this.Controls.Add(this.txtToUpper);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "frmMain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtToUpper;
+        private System.Windows.Forms.TextBox txtToLower;
+        private System.Windows.Forms.TextBox txtSearchChar;
+        private System.Windows.Forms.TextBox txtLastChar;
+        private System.Windows.Forms.TextBox txtStartIndex;
+        private System.Windows.Forms.TextBox txtRemove;
+        private System.Windows.Forms.TextBox txtReplaceChars;
+        private System.Windows.Forms.TextBox txtReplaceResult;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txtSearchIndex;
+        private System.Windows.Forms.TextBox txtLastIndexOf;
+        private System.Windows.Forms.TextBox txtEndIndex;
+        private System.Windows.Forms.TextBox txtSubstringResult;
+        private System.Windows.Forms.TextBox txtRemoveResult;
+        private System.Windows.Forms.TextBox txtReplaceWith;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Chapter05StringMethods.zip/Chapter05StringMethods.zip/frmMain.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter05StringMethods.zip
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool flag;
+            int index;
+            int start;
+            int HowMany;
+            string temp;
+
+            lblIndexOf.Text = " ";
+            txtLength.Text = txtInput.Text.Length.ToString();
+            
+            txtToUpper.Text = txtInput.Text.ToUpper();
+            txtToLower.Text = txtInput.Text.ToLower();
+
+            index = txtInput.Text.IndexOf(txtSearchChar.Text, 0);
+            lblIndexOf.Text = "txt.Input.Text.LastIndexOf(\"" + txtLastChar.Text + "\") = ";
+            txtLastIndexOf.Text = index.ToString();
+            
+            flag = int.TryParse(txtStartIndex.Text, out start);
+            if (flag == false) 
+            {
+                MessageBox.Show("Improper nemric input. Re-enter.");
+                txtStartIndex.Focus();
+                return;
+            }
+            
+            flag = int.TryParse(txtEndIndex.Text, out HowMany);
+            if (flag == false) 
+            {
+                MessageBox.Show("Improper nemric input. Re-enter.");
+                txtEndIndex.Focus();
+                return;
+            }
+
+            lblSubstring.Text = "txt.Input.Text.Substring(" + start.ToString() + ", " +
+                HowMany.ToString() + ") = ";
+            txtSubstringResult.Text = txtInput.Text.Substring(start, HowMany);
+
+            temp = txtInput.Text;
+            index = temp.IndexOf(txtRemove.Text);
+            if (index > 0) 
+            {
+                txtRemoveResult.Text = temp.Remove(index, txtRemove.Text.Length);
+            }
+
+            temp = txtInput.Text;
+            txtReplaceResult.Text = temp.Replace(txtReplaceChars.Text, txtReplaceWith.Text);
+        }
+    }
+}
