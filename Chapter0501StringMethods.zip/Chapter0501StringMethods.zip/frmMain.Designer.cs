e5a25ff3ad529da7152135151c33c45d8c041351
rjmermaid <bashenovjr@gmail.com>
2021-04-15 05:49:14 +0600

    Created DateTime Program

---
+++ Chapter0501StringMethods.zip/Chapter0501StringMethods.zip/frmMain.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace Chapter0501StringMethods.zip
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFull = new System.Windows.Forms.TextBox();
+            this.txtLong = new System.Windows.Forms.TextBox();
+            this.txtShort = new System.Windows.Forms.TextBox();
+            this.txtGeneral = new System.Windows.Forms.TextBox();
+            this.txtLongTime = new System.Windows.Forms.TextBox();
+            this.txtShortTime = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNewYears = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(31, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Full Date and Time:";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(31, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Long Date:";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(31, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Short Date:";
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(31, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "General Date and Time:";
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(31, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Long Time:";
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Location = new System.Drawing.Point(31, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 23);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Short Time:";
+            // 
+            // txtFull
+            // 
+            this.txtFull.Location = new System.Drawing.Point(170, 25);
+            this.txtFull.Name = "txtFull";
+            this.txtFull.Size = new System.Drawing.Size(144, 20);
+            this.txtFull.TabIndex = 6;
+            // 
+            // txtLong
+            // 
+            this.txtLong.Location = new System.Drawing.Point(170, 51);
+            this.txtLong.Name = "txtLong";
+            this.txtLong.Size = new System.Drawing.Size(144, 20);
+            this.txtLong.TabIndex = 7;
+            // 
+            // txtShort
+            // 
+            this.txtShort.Location = new System.Drawing.Point(170, 81);
+            this.txtShort.Name = "txtShort";
+            this.txtShort.Size = new System.Drawing.Size(144, 20);
+            this.txtShort.TabIndex = 8;
+            // 
+            // txtGeneral
+            // 
+            this.txtGeneral.Location = new System.Drawing.Point(170, 112);
+            this.txtGeneral.Name = "txtGeneral";
+            this.txtGeneral.Size = new System.Drawing.Size(144, 20);
+            this.txtGeneral.TabIndex = 9;
+            // 
+            // txtLongTime
+            // 
+            this.txtLongTime.Location = new System.Drawing.Point(170, 139);
+            this.txtLongTime.Name = "txtLongTime";
+            this.txtLongTime.Size = new System.Drawing.Size(144, 20);
+            this.txtLongTime.TabIndex = 10;
+            // 
+            // txtShortTime
+            // 
+            this.txtShortTime.Location = new System.Drawing.Point(170, 171);
+            this.txtShortTime.Name = "txtShortTime";
+            this.txtShortTime.Size = new System.Drawing.Size(144, 20);
+            this.txtShortTime.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "&Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(214, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "&Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Location = new System.Drawing.Point(31, 200);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 23);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Days Until New Years:";
+            // 
+            // txtNewYears
+            // 
+            this.txtNewYears.Location = new System.Drawing.Point(170, 200);
+            this.txtNewYears.Name = "txtNewYears";
+            this.txtNewYears.Size = new System.Drawing.Size(144, 20);
+            this.txtNewYears.TabIndex = 15;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(124, 248);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "C&alculate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 283);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtNewYears);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtShortTime);
+            this.Controls.Add(this.txtLongTime);
+            this.Controls.Add(this.txtGeneral);
+            this.Controls.Add(this.txtShort);
+            this.Controls.Add(this.txtLong);
+            this.Controls.Add(this.txtFull);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Date Time Tester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFull;
+        private System.Windows.Forms.TextBox txtLong;
+        private System.Windows.Forms.TextBox txtShort;
+        private System.Windows.Forms.TextBox txtGeneral;
+        private System.Windows.Forms.TextBox txtLongTime;
+        private System.Windows.Forms.TextBox txtShortTime;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNewYears;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Chapter0501StringMethods.zip/Chapter0501StringMethods.zip/frmMain.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter0501StringMethods.zip
+{
+    public partial class frmMain : Form
+    {
+        
+        public frmMain()
+        {
+            InitializeComponent();
+            UpdateTimeInfo(); 
+        }
+        private void UpdateTimeInfo()
+        {
+            int Days;
+            DateTime myTime = new DateTime();
+            myTime = DateTime.UtcNow;
+            DateTime newYears = new DateTime(myTime.Year, 12, 31);
+
+            txtFull.Text = myTime.ToString("f");
+            txtLong.Text = myTime.ToString("D");
+            txtShort.Text = myTime.ToString("d");
+            txtLongTime.Text = myTime.ToString("T");
+            txtShortTime.Text = myTime.ToString("t");
+            txtGeneral.Text = myTime.ToString("g");
+
+            Days = newYears.DayOfYear - myTime.DayOfYear;
+            txtNewYears.Text = Days.ToString();
+        }
+
+        private void UpdateTimeInfo(int year, int month, int day)
+        {   
+            int Days;
+            DateTime myTime = new DateTime(year, month, day);
+            myTime = DateTime.UtcNow;
+            DateTime newYears = new DateTime(myTime.Year, 12, 31);
+
+            txtFull.Text = myTime.ToString("f");
+            txtLong.Text = myTime.ToString("D");
+            txtShort.Text = myTime.ToString("d");
+            txtLongTime.Text = myTime.ToString("T");
+            txtShortTime.Text = myTime.ToString("t");
+            txtGeneral.Text = myTime.ToString("g");
+
+            Days = newYears.DayOfYear - myTime.DayOfYear;
+            txtNewYears.Text = Days.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            UpdateTimeInfo(2008, 6, 6);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateTimeInfo();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
