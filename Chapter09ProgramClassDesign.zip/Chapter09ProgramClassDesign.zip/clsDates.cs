691d938c7798a971c9f6e7119968acc65c88c9ba
rjmermaid <bashenovjr@gmail.com>
2021-04-22 05:01:54 +0600

    Finished Chapter9 TryItOuts

---
+++ Chapter09ProgramClassDesign.zip/Chapter09ProgramClassDesign.zip/clsDates.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter09ProgramClassDesign.zip
+{
+    class clsDates
+    {
+        private static int[] daysInMonth =
+        {
+             0, 31, 28, 31, 30, 31, 30, 31,
+            31, 30, 31, 30, 31
+        };
+        private int day;
+        private int month;
+        private int year;
+        private int leapYear;
+        private DateTime current;
+        public clsDates()
+        {
+            current = DateTime.UtcNow;
+        }
+        public int getLeapYear(int year)
+        {
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public string getEaster(int year)
+        {
+            int offset;
+            int leap;
+            int day;
+            int temp1;
+            int temp2;
+            int total;
+            offset = year % 19;
+            leap = year % 4;
+            day = year % 7;
+            temp1 = (19 * offset + 24) % 30;
+            temp2 = (2 * leap + 4 * day + 5) % 7;
+            total = 22 + temp2 + temp1;
+            if (total > 31)
+            {
+                month = 4;
+                day = total - 31;
+            }
+            else
+            {
+                month = 3;
+                day = total;
+            }
+            DateTime myDT = new DateTime(year, month, day);
+            return myDT.ToLongDateString();
+        }
+    }
+}
---
+++ Chapter09ProgramClassDesign.zip/Chapter09ProgramClassDesign.zip/frmMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter09ProgramClassDesign.zip
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool flag;
+            int year;
+            int leap;
+            clsDates myDate = new clsDates();
+            flag = int.TryParse(textBox1.Text, out year);
+            if (flag == false)
+            {
+                MessageBox.Show("Digits characters only in YYYY format.", "Input Error");
+                textBox1.Focus();
+                return;
+            }
+            leap = myDate.getLeapYear(year);
+            label2.Text = year.ToString() + " is " + ((leap == 1) ? "":"not ") + "a leap year";
+            label3.Text = myDate.getEaster(year);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
